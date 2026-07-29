d4e3224abf3157c4951d704f70d9858d566bba47
Irfan Yasin <irfanyasin1303@gmail.com>
2026-07-29 23:58:53 +0700

    feat: remove proxy

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,39 @@
-import { defineConfig } from 'vite';
-import fs from 'fs';
-import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import fs from "fs";
+import laravel from "laravel-vite-plugin";
+import { bunny } from "laravel-vite-plugin/fonts";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
             fonts: [
-                bunny('Instrument Sans', {
+                bunny("Instrument Sans", {
                     weights: [400, 500, 600],
                 }),
             ],
         }),
         tailwindcss(),
     ],
-    server: {
-        host: 'localhost',
-        https: {
-            key: fs.readFileSync('./certs/localhost-key.pem'),
-            cert: fs.readFileSync('./certs/localhost.pem'),
-        },
-        // Proxy semua request ke Laravel (php artisan serve)
-        proxy: {
-            // Kecualikan asset Vite, teruskan sisanya ke Laravel
-            '^(?!/@vite|/resources|/@fs|/node_modules)': {
-                target: 'http://127.0.0.1:8000',
-                changeOrigin: true,
-                secure: false,
-            },
-        },
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
+    // server: {
+    //     host: 'localhost',
+    //     https: {
+    //         key: fs.readFileSync('./certs/localhost-key.pem'),
+    //         cert: fs.readFileSync('./certs/localhost.pem'),
+    //     },
+    //     // Proxy semua request ke Laravel (php artisan serve)
+    //     proxy: {
+    //         // Kecualikan asset Vite, teruskan sisanya ke Laravel
+    //         '^(?!/@vite|/resources|/@fs|/node_modules)': {
+    //             target: 'http://127.0.0.1:8000',
+    //             changeOrigin: true,
+    //             secure: false,
+    //         },
+    //     },
+    //     watch: {
+    //         ignored: ['**/storage/framework/views/**'],
+    //     },
+    // },
 });
